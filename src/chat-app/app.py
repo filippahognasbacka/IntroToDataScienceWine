624173827a32e9109f294ca418d477baab6d639b
aarekr <aare212@hotmail.com>
2025-10-03 20:03:55 +0300

    Web app and simple ML combined, requirements update

--- src/chat-app/app.py
+++ src/chat-app/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask import render_template
+from embeddings_simple import Embeddings
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    embeddings = Embeddings()
+    embeddings.load_data()
+    embeddings.load_embeddings()
+    query = input("Enter a query: ")
+    recommendations = embeddings.recommend_wines(query)
+    print(recommendations)
+    return render_template("index.html", recommendations=recommendations)
---
+++ src/chat-app/embeddings_simple.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+class Embeddings:
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.df = None
+        self.embeddings = None
+
+    def load_data(self):
+        #df = pd.read_csv("/path/to/wine-review-dataset/winemag-data-130k-v2.csv")
+        df = pd.read_csv("C:/Users/aarek/OneDrive/Skrivbord/cleaned_version.csv")
+        df = df.dropna(subset=["description"])
+
+        self.df = df
+
+    def load_embeddings(self):
+        self.embeddings = self.model.encode(self.df['description'].tolist(), show_progress_bar=True)
+
+        print("Loaded embeddings with shape: ", self.embeddings.shape)
+        print(self.embeddings[0])
+
+    def recommend_wines(self, query: str, top_n: int = 5):
+        query_embedding = self.model.encode([query])
+        similarities = cosine_similarity(query_embedding, self.embeddings)[0]
+        
+        top_indices = np.argsort(similarities)[::-1][:top_n]
+
+        return self.df.iloc[top_indices][["country", "variety", "winery", "points", "description"]]
